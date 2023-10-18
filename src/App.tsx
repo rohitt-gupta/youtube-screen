05d71103a54560c8b017a9b1d028224a549497ca
rohitGupta2022 <pta.rohit28@gmail.com>
2023-10-18 18:37:15 +0530

    Add: Small/screen sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import PageHeader from './layouts/PageHeader'
 import CategoryPills from './components/CategoryPills'
 import { categories, videos } from './data/home'
 import { VideoGridItem } from './components/VideoGridItem'
+import SideBar from './layouts/SideBar'
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(categories[0])
@@ -10,7 +11,7 @@ const App = () => {
     <div className='max-h-screen flex flex-col'>
       <PageHeader />
       <div className='grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto'>
-        <div>SIDEBAR</div>
+        <SideBar />
         <div className='overflow-x-hidden px-8 pb-4'>
           <div className='sticky top-0 bg-white z-10 pb-4'>
             <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory} />
